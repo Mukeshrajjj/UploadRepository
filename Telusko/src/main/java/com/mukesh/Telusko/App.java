ca8136ae17d939340c948117b2977f14c71802ec
Lenovo <Lenovo@DESKTOP-G4V2RO8>
2020-06-20 17:00:03 +0530

    Initial Commit

---
+++ Telusko/src/main/java/com/mukesh/Telusko/App.java
@@ -0,0 +1,27 @@
+package com.mukesh.Telusko;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+    public static void main( String[] args )
+    {
+        ArrayList<String> arrList=new ArrayList<String>();
+        arrList.add("a");
+        arrList.add("b");
+        arrList.add("c");
+        System.out.println(arrList);
+        arrList.toArray();
+        System.out.println(arrList);     
+        
+    }
+}
